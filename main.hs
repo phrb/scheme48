e7cc29dccb7401a8f6eb0b5d96dad8a25f88ac32
phrb <pedro.bruel@gmail.com>
2015-04-05 14:57:58 -0300

    Solving exercises.

--- main.hs
+++ main.hs
@@ -10,12 +10,24 @@ data LispVal = Atom String
              | Number Integer
              | String String
              | Bool Bool
+             | Character Char 
 
 -- Defining a symbol parser
 symbol :: Parser Char
-symbol = oneOf "!#$%&|*+-/:<=>?@^_~"
+symbol = oneOf "!$%&|*+-/:<=>?@^_~"
 
--- Defining a whitespace parser
+-- Defining an escaped char parser
+escapedChars :: Parser Char
+escapedChars = do char '\\' -- a backslash
+                  x <- oneOf "\\\"nrt" -- either backslash or doublequote
+                  return $ case x of
+                    '\\' -> x
+                    '"'  -> x
+                    'n'  -> '\n'
+                    'r'  -> '\r'
+                    't'  -> '\t'
+
+--Defining a whitespace parser
 spaces :: Parser ()
 spaces = skipMany1 space
 
@@ -23,9 +35,14 @@ spaces = skipMany1 space
 parseString :: Parser LispVal
 parseString = do
                 char '"'
-                x <- many (noneOf "\"")
+                x <- many $ escapedChars <|> noneOf "\"\\"
                 char '"'
                 return $ String x
+
+parseBool :: Parser LispVal
+parseBool = do
+              char '#'
+              (char 't' >> return (Bool True)) <|> (char 'f' >> return (Bool False))
 
 -- Defining an Atom parser
 parseAtom :: Parser LispVal
@@ -33,20 +50,58 @@ parseAtom = do
               first <- letter <|> symbol
               rest <- many (letter <|> digit <|> symbol)
               let atom = first:rest
-              return $ case atom of
-                         "#t" -> Bool True
-                         "#f" -> Bool False
-                         _    -> Atom atom
+              return $ Atom atom
 
 -- Defining a Number parser
 parseNumber :: Parser LispVal
-parseNumber = many1 digit >>= (return . Number . read)
+parseNumber = parseDigital1
+                <|> parseDigital2
+                <|> parseHex
+                <|> parseOct
+                <|> parseBin
+
+-- Old style parser
+parseDigital1 :: Parser LispVal
+parseDigital1 = many1 digit >>= (return . Number . read)
+
+-- New style parser
+parseDigital2 :: Parser LispVal
+parseDigital2 = do try $ string "#d"
+                   x <- many1 digit
+                   (return . Number . read) x
+
+-- Hex parser
+parseHex :: Parser LispVal
+parseHex = do try $ string "#x"
+              x <- many1 hexDigit
+              return $ Number (hex2dig x)
+
+-- Oct parser
+parseOct :: Parser LispVal
+parseOct = do try $ string "#o"
+              x <- many1 octDigit
+              return $ Number (oct2dig x)
+
+-- Bin parser
+parseBin :: Parser LispVal
+parseBin = do try $ string "#b"
+              x <- many1 (oneOf "10")
+              return $ Number (bin2dig x)
+
+-- Helper functions
+oct2dig x = fst $ readOct x !! 0
+hex2dig x = fst $ readHex x !! 0
+bin2dig  = bin2dig' 0
+bin2dig' digint "" = digint
+bin2dig' digint (x:xs) = let old = 2 * digint + (if x == '0' then 0 else 1) in
+                         bin2dig' old xs
 
 -- Defining a separate exp parser
 parseExpr :: Parser LispVal
 parseExpr = parseAtom
          <|> parseString
          <|> parseNumber
+         <|> parseBool
 
 -- Parsing and treating errors
 readExpr :: String -> String
